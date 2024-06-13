6852c212b10a16c40e9aafae486b56492f21aa38
Trushkova Mary <94931275+truefalsemary@users.noreply.github.com>
2024-06-14 01:51:05 +0300

    feat: add route modal

--- lib/route_modal.dart
+++ lib/route_modal.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:green_app/modal_body.dart';
 
 class RouteModalBody extends StatelessWidget {
@@ -9,35 +11,61 @@ class RouteModalBody extends StatelessWidget {
     return ModalBody(
       title: 'Оцените маршрут',
       bodyWidget: [
-        const QuestionBlocWidget(
+        QuestionBlocWrapper(
           question: 'Как бы Вы оценили живописность маршрута?',
           count: 5,
         ),
-        const QuestionBlocWidget(
+        QuestionBlocWrapper(
           question: 'Как бы Вы оценили чистоту маршрута?',
           count: 5,
         ),
-        const QuestionBlocWidget(
+        QuestionBlocWrapper(
           question:
               'Как бы Вы оценили обустроенность маршрута (указатели, информационные аншлаги)?',
           count: 5,
         ),
-        const QuestionBlocWidget(
-          question:
-              'Насколько удобным и комфортным было размещение на маршруте (если было)?',
-          count: 5,
-        ),
-        const QuestionBlocWidget(
+        QuestionBlocWrapper(
           question: 'Насколько соответствовали ожидания Вашему опыту?',
           count: 5,
         ),
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text(
-            'Отправить оценку',
+        SizedBox(
+          height: 10,
+        ),
+        Align(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                backgroundColor: Theme.of(context).primaryColor,
+                foregroundColor: Colors.white.withOpacity(0.9)),
+            onPressed: () => Navigator.pop(context),
+            child: const Text(
+              'Отправить оценку',
+            ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class QuestionBlocWrapper extends StatelessWidget {
+  const QuestionBlocWrapper({
+    super.key,
+    required this.question,
+    required this.count,
+  });
+
+  final String question;
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return QuestionBlocWidget(
+      question: question,
+      count: count,
+      activeBackColor: Theme.of(context).primaryColor,
+      inactiveTextColor: Colors.black,
     );
   }
 }
@@ -49,10 +77,10 @@ class QuestionBlocWidget extends StatefulWidget {
     required this.count,
     this.onChanged,
     this.padding,
-    this.activeBackColor = Colors.yellow,
-    this.activeTextColor = Colors.green,
-    this.inactiveBackColor = Colors.green,
-    this.inactiveTextColor = Colors.black,
+    this.activeBackColor = Colors.green,
+    this.activeTextColor = Colors.white,
+    this.inactiveBackColor = const Color(0xFF7EAB83),
+    this.inactiveTextColor = Colors.green,
   });
 
   final String question;
@@ -76,6 +104,11 @@ class QuestionBlocWidget extends StatefulWidget {
 class _QuestionBlocWidgetState extends State<QuestionBlocWidget> {
   late int currentValue;
 
+  // late final Color activeBackColor;
+  // late final Color activeTextColor;
+  // late final Color inactiveBackColor;
+  // late final Color inactiveTextColor;
+
   @override
   void initState() {
     currentValue = 1;
@@ -83,9 +116,15 @@ class _QuestionBlocWidgetState extends State<QuestionBlocWidget> {
   }
 
   @override
+  void didUpdateWidget(covariant QuestionBlocWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: widget.padding ?? const EdgeInsets.symmetric(vertical: 8),
+      padding: widget.padding ?? const EdgeInsets.symmetric(vertical: 10),
       child: Column(
         children: [
           Center(
@@ -93,7 +132,9 @@ class _QuestionBlocWidgetState extends State<QuestionBlocWidget> {
             widget.question,
             textAlign: TextAlign.center,
           )),
-          // SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           SizedBox(
             height: 60,
             child: ListView.separated(
@@ -115,9 +156,13 @@ class _QuestionBlocWidgetState extends State<QuestionBlocWidget> {
                   child: Container(
                     width: 60,
                     // padding: const EdgeInsets.all(12),
-                    color: idx + 1 == currentValue
-                        ? widget.activeBackColor
-                        : widget.inactiveBackColor,
+                    decoration: BoxDecoration(
+                      color: idx + 1 == currentValue
+                          ? widget.activeBackColor
+                          : widget.inactiveBackColor,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
                     child: Center(
                       child: Text(
                         '${idx + 1}',
